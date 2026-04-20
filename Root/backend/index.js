1c05db0d8fd749255f0db0397da7da90db86d160
congkiet05 <congkiet05@gmail.com>
2026-04-20 20:57:50 +0700

    khởi tạo dự án

---
+++ Root/backend/index.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
